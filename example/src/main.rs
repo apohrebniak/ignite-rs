3eca894dc05beb53958f744c0b2a6eda8c5c6e1d
Andrii Pohrebniak <a.pohrebniak@gethatch.com>
2020-04-20 15:05:40 +0300

    Modules. Proper message reading logic

--- example/src/main.rs
+++ example/src/main.rs
@@ -3,4 +3,8 @@ use ignite_rs::Ignite;
 fn main() {
     let ignite = Ignite::new(String::from("localhost:10800")).unwrap();
     let conn = ignite.get_new_connection();
+
+    if let Ok(_) = conn {
+        println!("Connection established!")
+    }
 }
--- ignite-rs/src/message.rs
+++ ignite-rs/src/message.rs
@@ -1,21 +1,69 @@
-use crate::error::IgniteResult;
+use crate::error::{IgniteError, IgniteResult};
+use crate::parser;
+use crate::parser::{Flag, OpCode};
+use std::io::{Error, Read};
 
-/// Basic trait implemented by all messages
-trait Request: Sized {
-    fn into_bytes(self) -> [u8];
-    fn read_on_success(&self) -> IgniteResult<Self>;
+pub(crate) struct ResponseHeader {
+    pub(crate) length: i32,
+    pub(crate) flag: Flag,
 }
 
-struct HandshakeRequest {}
+impl ResponseHeader {
+    pub(crate) fn read_header<T: Read>(reader: &mut T) -> IgniteResult<ResponseHeader> {
+        match parser::read_i32_le(reader) {
+            Ok(len) => {
+                if len > 0 {
+                    match parser::read_u8(reader) {
+                        Ok(flag) => match flag {
+                            1 => Ok(ResponseHeader {
+                                length: len,
+                                flag: Flag::Success,
+                            }),
+                            _ => Ok(ResponseHeader {
+                                length: len,
+                                flag: Flag::Failure,
+                            }),
+                        },
+                        Err(err) => Err(IgniteError::from(err)),
+                    }
+                } else {
+                    return Err(IgniteError {});
+                }
+            }
+            Err(err) => Err(IgniteError::from(err)),
+        }
+    }
+}
 
-// impl Request for HandshakeRequest {
-//     fn into_bytes(self) -> [u8] {
-//         unimplemented!()
-//     }
-//
-//     fn read_on_success(&self) -> IgniteResult<Self> {
-//
-//         reader.read_u8_le();
-//         reader.read_i32_le();
-//     }
-// }
+pub(crate) struct HandshakeRequest<'a> {
+    pub major_v: i16,
+    pub minor_v: i16,
+    pub patch_v: i16,
+    pub username: Option<&'a str>,
+    pub password: Option<&'a str>,
+}
+
+impl Into<Vec<u8>> for HandshakeRequest<'_> {
+    fn into(self) -> Vec<u8> {
+        let mut data = Vec::<u8>::new();
+        data.push(OpCode::Handshake as u8);
+        data.append(&mut i16::to_le_bytes(self.major_v).to_vec());
+        data.append(&mut i16::to_le_bytes(self.minor_v).to_vec());
+        data.append(&mut i16::to_le_bytes(self.patch_v).to_vec());
+        data.push(2); //client code
+                      // // if let Some(x) = self.username { //TODO: implement
+                      // //     bytes.append(x.as_bytes());
+                      // // }
+                      // // if let Some() { }
+
+        // get the overall message length
+        let len = data.len() as i32;
+
+        // insert length in the begging of message
+        let mut bytes = Vec::new();
+        bytes.append(&mut i32::to_le_bytes(len).to_vec());
+        bytes.extend_from_slice(data.as_slice());
+
+        bytes
+    }
+}
