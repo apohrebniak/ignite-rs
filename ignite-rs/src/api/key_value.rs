094b9a7d066ceac2b5b6683be2abbfee3da5c638
Andrii Pohrebniak <a.pohrebniak@gethatch.com>
2020-05-13 11:18:32 +0300

    Added some KV operations

--- ignite-rs/src/api/key_value.rs
+++ ignite-rs/src/api/key_value.rs
@@ -1,10 +1,15 @@
-use crate::Pack;
+use crate::cache::CachePeekMode;
+use crate::error::IgniteResult;
+use crate::protocol::{pack_i32, pack_u8, read_data_obj};
+use crate::{Pack, PackType, Unpack, UnpackType};
+use std::any::Any;
+use std::io::Read;
 
-pub(crate) enum CacheReq<K: Pack, V: Pack> {
+pub(crate) enum CacheReq<K: PackType, V: PackType> {
     Get(i32, K),
     GetAll(i32, Vec<K>),
     Put(i32, K, V),
-    PutAll(i32, Vec<(i32, K, V)>),
+    PutAll(i32, Vec<(K, V)>),
     ContainsKey(i32, K),
     ContainsKeys(i32, Vec<K>),
     GetAndPut(i32, K, V),
@@ -19,35 +24,100 @@ pub(crate) enum CacheReq<K: Pack, V: Pack> {
     ClearKeys(i32, Vec<K>),
     RemoveKey(i32, K),
     RemoveIfEquals(i32, K, V),
-    GetSize(i32), //TODO
+    GetSize(i32, Vec<CachePeekMode>),
     RemoveKeys(i32, Vec<K>),
     RemoveAll(i32),
 }
 
-impl<K: Pack, V: Pack> Pack for CacheReq<K, V> {
+impl<K: PackType, V: PackType> Pack for CacheReq<K, V> {
     fn pack(self) -> Vec<u8> {
         match self {
             CacheReq::Get(id, key)
             | CacheReq::ContainsKey(id, key)
             | CacheReq::GetAndRemove(id, key)
             | CacheReq::ClearKey(id, key)
-            | CacheReq::RemoveKey(id, key) => {}
-            CacheReq::GetAll(id, key)
-            | CacheReq::ContainsKeys(id, key)
-            | CacheReq::ClearKeys(id, key)
-            | CacheReq::RemoveKeys(id, key) => {}
+            | CacheReq::RemoveKey(id, key) => {
+                let mut bytes: Vec<u8> = Vec::new();
+                bytes.append(&mut pack_i32(id));
+                bytes.append(&mut pack_u8(0)); //magic
+                bytes.append(&mut key.pack());
+                bytes
+            }
+            CacheReq::GetAll(id, keys)
+            | CacheReq::ContainsKeys(id, keys)
+            | CacheReq::ClearKeys(id, keys)
+            | CacheReq::RemoveKeys(id, keys) => {
+                let mut bytes: Vec<u8> = Vec::new();
+                bytes.append(&mut pack_i32(id));
+                bytes.append(&mut pack_u8(0)); //magic
+                bytes.append(&mut pack_i32(keys.len() as i32));
+                for k in keys {
+                    bytes.append(&mut k.pack());
+                }
+                bytes
+            }
             CacheReq::Put(id, key, value)
             | CacheReq::GetAndPut(id, key, value)
             | CacheReq::GetAndReplace(id, key, value)
             | CacheReq::PutIfAbsent(id, key, value)
             | CacheReq::GetAndPutIfAbsent(id, key, value)
             | CacheReq::Replace(id, key, value)
-            | CacheReq::RemoveIfEquals(id, key, value) => {}
-            CacheReq::PutAll(_id, _pairs) => {}
-            CacheReq::ReplaceIfEquals(_id, _, _, _) => {}
-            CacheReq::Clear(id) | CacheReq::RemoveAll(id) => {}
-            CacheReq::GetSize(_id) => {}
+            | CacheReq::RemoveIfEquals(id, key, value) => {
+                let mut bytes: Vec<u8> = Vec::new();
+                bytes.append(&mut pack_i32(id));
+                bytes.append(&mut pack_u8(0)); //magic
+                bytes.append(&mut key.pack());
+                bytes.append(&mut value.pack());
+                bytes
+            }
+            CacheReq::PutAll(id, pairs) => {
+                let mut bytes: Vec<u8> = Vec::new();
+                bytes.append(&mut pack_i32(id));
+                bytes.append(&mut pack_u8(0)); //magic
+                bytes.append(&mut pack_i32(pairs.len() as i32));
+                for pair in pairs {
+                    bytes.append(&mut pair.0.pack());
+                    bytes.append(&mut pair.1.pack());
+                }
+                bytes
+            }
+            CacheReq::ReplaceIfEquals(id, key, old, new) => {
+                let mut bytes: Vec<u8> = Vec::new();
+                bytes.append(&mut pack_i32(id));
+                bytes.append(&mut pack_u8(0)); //magic
+                bytes.append(&mut key.pack());
+                bytes.append(&mut old.pack());
+                bytes.append(&mut new.pack());
+                bytes
+            }
+            CacheReq::Clear(id) | CacheReq::RemoveAll(id) => {
+                let mut bytes: Vec<u8> = Vec::new();
+                bytes.append(&mut pack_i32(id));
+                bytes.append(&mut pack_u8(0)); //magic
+                bytes
+            }
+            CacheReq::GetSize(id, modes) => {
+                let mut bytes: Vec<u8> = Vec::new();
+                bytes.append(&mut pack_i32(id));
+                bytes.append(&mut pack_u8(0)); //magic
+                bytes.append(&mut pack_i32(0));
+                for mode in modes {
+                    bytes.append(&mut pack_u8(mode.into()));
+                }
+                bytes
+            }
         }
-        Vec::new()
+    }
+}
+
+pub(crate) struct CacheGetResp<T: UnpackType> {
+    pub(crate) val: Option<T>,
+}
+
+impl<T: UnpackType> Unpack for CacheGetResp<T> {
+    fn unpack(reader: &mut impl Read) -> IgniteResult<Box<Self>> {
+        let val = T::unpack(reader)?;
+        let val = val.map(|v| *v);
+        Ok(Box::new(CacheGetResp { val }))
     }
 }
