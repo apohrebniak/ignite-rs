b9e792053dd453ca39c1062dd58924315de0c4c9
Brent Gardner <brent.gardner@spaceandtime.io>
2023-06-01 16:21:03 -0600

    Working RTT

--- ignite-rs/src/protocol/complex_obj.rs
+++ ignite-rs/src/protocol/complex_obj.rs
@@ -1,8 +1,8 @@
 use crate::error::{IgniteError, IgniteResult};
 use crate::protocol::{
-    read_i16, read_i32, read_string, read_u16, read_u8, write_i32, write_i64,
-    write_string, write_u16, write_u8, TypeCode, COMPLEX_OBJ_HEADER_LEN, FLAG_COMPACT_FOOTER,
-    FLAG_HAS_SCHEMA, FLAG_OFFSET_ONE_BYTE, FLAG_OFFSET_TWO_BYTES, FLAG_USER_TYPE, HAS_RAW_DATA,
+    read_i16, read_i32, read_string, read_u16, read_u8, write_i32, write_i64, write_string,
+    write_u16, write_u8, TypeCode, COMPLEX_OBJ_HEADER_LEN, FLAG_COMPACT_FOOTER, FLAG_HAS_SCHEMA,
+    FLAG_OFFSET_ONE_BYTE, FLAG_OFFSET_TWO_BYTES, FLAG_USER_TYPE, HAS_RAW_DATA,
 };
 use crate::utils::{bytes_to_java_hashcode, get_schema_id, string_to_java_hashcode};
 use crate::{ReadableType, WritableType};
@@ -65,7 +65,7 @@ impl ReadableType for ComplexObject {
                 let flags = read_u16(&mut header)?; // offset 2
                 let _type_id = read_i32(&mut header)?; // offset 4
                 let _hash_code = read_i32(&mut header)?; // offset 8
-                let _object_len = read_i32(&mut header)? as usize; // offset 12
+                let object_len = read_i32(&mut header)? as usize; // offset 12
                 let _schema_id = read_i32(&mut header)?; // offset 16
                 let fields_offset_offset = read_i32(&mut header)? as usize; // offset 20
 
@@ -83,10 +83,9 @@ impl ReadableType for ComplexObject {
                     (false, true) => 2,
                     _ => 4, // https://apacheignite.readme.io/docs/binary-client-protocol-data-format#schema
                 };
-                let true_obj_len = fields_offset_offset + offset_sz;
 
                 // append body
-                let mut body = vec![0u8; true_obj_len - data.len()];
+                let mut body = vec![0u8; object_len - data.len()];
                 reader.read_exact(&mut body)?;
                 data.extend(body);
 
@@ -104,12 +103,7 @@ impl ReadableType for ComplexObject {
                     };
                     me.values.push(val);
                 }
-                let _raw_data_offset = match offset_sz {
-                    1 => read_u8(&mut remainder)? as usize,
-                    2 => read_i16(&mut remainder)? as usize,
-                    4 => read_i32(&mut remainder)? as usize,
-                    _ => Err(IgniteError::from("Invalid offset size!"))?,
-                };
+                // the remainder of bytes are offsets of offset_sz to the fields
             }
             _ => todo!("Missing type code"),
         }
@@ -135,17 +129,21 @@ impl WritableType for ComplexObject {
 
         // write fields to vec so we can hash
         let mut fields: Vec<u8> = Vec::new();
+        let mut offsets: Vec<u8> = Vec::new();
         for val in &self.values {
             match val {
                 IgniteValue::String(val) => {
+                    offsets.push(COMPLEX_OBJ_HEADER_LEN as u8 + fields.len() as u8);
                     write_u8(&mut fields, TypeCode::String as u8)?;
                     write_string(&mut fields, val)?
-                },
+                }
                 IgniteValue::Long(val) => {
+                    offsets.push(COMPLEX_OBJ_HEADER_LEN as u8 + fields.len() as u8);
                     write_u8(&mut fields, TypeCode::Long as u8)?;
                     write_i64(&mut fields, *val)?;
                 }
                 IgniteValue::Int(val) => {
+                    offsets.push(COMPLEX_OBJ_HEADER_LEN as u8 + fields.len() as u8);
                     write_u8(&mut fields, TypeCode::Int as u8)?;
                     write_i32(&mut fields, *val)?;
                 }
@@ -162,9 +160,9 @@ impl WritableType for ComplexObject {
         write_i32(writer, bytes_to_java_hashcode(fields.as_slice()))?; // hash - offset 8
         write_i32(writer, self.size() as i32)?; // size - offset 12
         write_i32(writer, get_schema_id(&self.schema.fields))?; // schema_id - offset 16
-        write_i32(writer, self.size() as i32 - 1)?; // offset to the offset to field data - 20
+        write_i32(writer, COMPLEX_OBJ_HEADER_LEN + fields.len() as i32)?; // offset to the offset to field data - 20
         writer.write_all(&fields)?; // field data - offset 24
-        write_u8(writer, COMPLEX_OBJ_HEADER_LEN as u8)?; // offset to fields - 34
+        writer.write_all(&offsets)?;
 
         Ok(())
     }
@@ -183,16 +181,16 @@ impl WritableType for ComplexObject {
                     IgniteValue::Int(_) => size_of::<i32>() + 1,
                 }
             });
-        size + 1
+        size + self.values.len()
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::convert::TryInto;
     use super::*;
     use crate::protocol::complex_obj::ComplexObject;
     use crate::{new_client, Ignite};
+    use std::convert::TryInto;
 
     #[test]
     fn test_round_trip() {
@@ -217,8 +215,8 @@ mod tests {
             "03 E9 03 00 00" // gas_limit = 1001
             "09 05 00 00 00 30 2E 30 30 31" // base_fee_per_gas = 0.001
             "03 43 00 00 00" // transaction_count = 67
-            
-            "18" // offset to data
+
+            "18 22 2C 36 40 4B 50 55 5A 64" // offsets to fields
         );
         let schema = ComplexObjectSchema {
             type_name: type_name.to_string(),
@@ -233,13 +231,15 @@ mod tests {
                 "GAS_LIMIT".to_string(),
                 "BASE_FEE_PER_GAS".to_string(),
                 "TRANSACTION_COUNT".to_string(),
-            ]
+            ],
         };
 
         // deserialize
         let mut reader = Cursor::new(expected_bytes);
         let type_code = read_u8(&mut reader).unwrap();
-        let mut val = ComplexObject::read_unwrapped(type_code.try_into().unwrap(), &mut reader).unwrap().unwrap();
+        let val = ComplexObject::read_unwrapped(type_code.try_into().unwrap(), &mut reader)
+            .unwrap()
+            .unwrap();
         let actual_values = format!("{:?}", val.values);
         let expected_values = r#"[String("0xabc"), String("11:00"), String("0xdef"), String("0x123"), String("0.0001"), Int(100), Int(999), Int(1001), String("0.001"), Int(67)]"#;
         assert_eq!(actual_values, expected_values);
