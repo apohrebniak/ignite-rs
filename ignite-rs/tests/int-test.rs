f7ff1574248f7e66f274c9eb57b033dbd3a4d701
Brent Gardner <brent.gardner@spaceandtime.io>
2023-06-09 11:00:15 -0600

    verify table exists

--- ignite-rs/tests/int-test.rs
+++ ignite-rs/tests/int-test.rs
@@ -1,7 +1,19 @@
 #[cfg(test)]
 mod int_test {
+    use ignite_rs::{ClientConfig, Ignite, new_client};
+
     #[test]
     fn sanity_test() {
         assert_eq!(true, true, "CI works");
     }
+
+    #[test]
+    fn should_list_caches() {
+        let config = ClientConfig::new("127.0.0.1:10800");
+        let mut ignite = new_client(config).unwrap();
+        let actual = ignite.get_cache_names().unwrap();
+        let expected = vec!["SQL_PUBLIC_RAINBOW"];
+        assert_eq!(actual, expected);
+    }
+
 }
